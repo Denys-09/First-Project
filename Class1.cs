7bc4428591bb1a7df7eaeb4875d5a45a44e0db43
4833527 <4833527@TE-GDA-D-D21E20>
2025-03-05 13:09:28 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Stairs
+{
+    static void PrintStairs(int times, int length, char symbol)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        PrintStairs(5, 4, '*');
+    }
+}
---
+++ First Project/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Write first number to multiply:");
+string num1 = Console.ReadLine();
+double answer1 = double.Parse(num1);
+Console.WriteLine("Write second number:");
+string num2 = Console.ReadLine();
+double answer2 = double.Parse(num2);
+double sum = answer1 + answer2;
+Console.WriteLine(answer1 + "+" + answer2 + "=" + sum);
